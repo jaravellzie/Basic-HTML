122973e00d79fce44adb6ef9c93a64fd82b1aad6
jaravellzie <53119278+jaravellzie@users.noreply.github.com>
2019-09-16 10:56:34 +0800

    Add files via upload

---
+++ LectureOne.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+<head>
+	<title>Lecture One</title>
+</head>
+<!-- Headings and Lists -->
+<body bgcolor="#5ba6ec">
+
+<!-- Heading List -->
+<h1 align="center">Header 1</h1>
+<h2 align="center">Header 2</h2>
+<h3 align="center">Header 3</h3>
+<h4 align="center">Header 4</h4>
+<h5 align="center">Header 5</h5>
+<h6 align="center">Header 6</h6>
+
+<!-- Paragraph Tag with font type, Color, and Size -->
+<font face="calibri" color="#3ebcfb" size="10">
+	<p>Paragraph<br>Tag</p>
+</font>
+
+<img src="darlene.jpg" height=550 width=500/>
+
+<ul type="square"> <!-- Unordered List -->
+	<li>One</li>
+	<li>Two</li>
+	<li>Three</li>
+</ul>
+
+<ol> <!-- Ordered List  -->
+	<li>One</li>
+	<li>Two</li>
+	<li>Three</li>
+</ol>
+
+<a href="LectureOne.php">Hypertext Reference</a>
+
+</body>
+</html>
